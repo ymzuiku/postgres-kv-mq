bb9ae5711a0103534cdba97ebfedb69a65579aa8
pillar <ymblender@gmail.com>
2023-04-18 07:39:52 +0800

    fix: add kv readme

--- packages/examples/main.ts
+++ packages/examples/main.ts
@@ -1,4 +1,4 @@
-import { kvex, pgConnect } from "postgres-kv";
+import { kvex, pgConnect, waiting } from "postgres-kv";
 import { mq } from "postgres-mq";
 import { config } from "up-dir-env";
 
@@ -8,30 +8,56 @@ kvex.config.clearInterval = 2000;
 pgConnect();
 
 mq.baseConfig({
+  workerConfig: {
+    // removeOnComplete: true,
+  },
   queueConfig: {
-    removeOnComplete: true,
-    removeOnFail: true,
+    // removeOnComplete: true,
+    // removeOnFail: true,
+    // removeOnCompleteDelay: 9000,
+    // removeOnCompleteRatio: 0.2,
+    // removeOnFailRatio: 0.2,
   },
 });
 
 let a = 0;
+let err = 0;
+
+const bigString = Array(9).fill("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").join(",");
 
 setTimeout(() => {
-  const timer = setInterval(() => {
+  const timer = setInterval(async () => {
     a += 1;
     if (a > 1000) {
       clearInterval(timer);
       return;
     }
 
-    mq.Queue("mq3", { name: "aaaaaaaaaaaa" });
+    if (a % 100 === 0) {
+      console.log("--debug--insert", a);
+    }
+    try {
+      await mq.Queue("mq13", { name: bigString });
+    } catch (e) {
+      err += 1;
+      console.log("--debug--err", err);
+      //
+    }
   });
 
-  mq.Worker("mq3", (job) => {
-    console.log("doing", job.id, job.data);
+  let n = 0;
+  mq.Worker("mq13", async (job) => {
+    await waiting(300);
+    n += 1;
+    if (n === 100) {
+      console.log("doing", job.id, job.data.name.length);
+    }
   });
 
-  // Worker.addListenerCompleted((job) => {
-  //   console.log("--debug--completed", job.id);
-  // });
-}, 1000);
+  mq.Worker.addListenerCompleted((job) => {
+    console.log("--debug--completed", job.id);
+  });
+  mq.Worker.addListenerFailed((err) => {
+    console.log("--debug--completed", err);
+  });
+}, 0);
